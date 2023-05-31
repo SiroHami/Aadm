a8365b93b1dea19786b33d5afafd734fdccca922
nanyeon99 <nanyeon99@gmail.com>
2023-05-31 23:49:02 +0900

    update

---
+++ datasets/CIFAR10_SSL_cls.py
@@ -0,0 +1,144 @@
+"""
+CIFAR10 full dataset
+"""
+
+import math
+import os
+import numpy as np
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from .dataset_info import DatasetInfo
+from PIL import Image
+
+
+class CIFAR10SSL(CIFAR10):
+    def __init__(self, root, indexs, train=True,
+                 transform=None, target_transform=None,
+                 download=False):
+        super().__init__(root, train=train,
+                         transform=transform,
+                         target_transform=target_transform,
+                         download=download)
+        if indexs is not None:
+            self.data = self.data[indexs]
+            self.targets = np.array(self.targets)[indexs]
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+
+class CIFAR10_SSL_Info(DatasetInfo):
+    def __init__(self):
+        super(CIFAR10_SSL_Info, self).__init__()
+        self.label = "CIFAR10_SSL"
+        self.num_labels = None
+        self.root_dir = "CIFAR10_SSL"
+        self.dataset_class = CIFAR10SSL
+        self.num_train_samples = 50000
+        self.in_channels = 3
+        self.num_classes = 10
+        self.input_size = (32, 32)
+        self.train_metric = ["Train.Error"]
+        self.train_metric_name = ["Top1Error"]
+        self.val_metric = ["Val.Error"]
+        self.val_metric_name = ["Top1Error"]
+        self.saver_acc_ind = 0
+        self.train_transfom = cifar10_train_transform
+        self.train_unlabel_transform = cifar10_train_transform
+        self.val_transfom = cifar10_val_transform
+        self.test_transfom = cifar10_val_transform
+
+
+def cifar10_train_transform(ds_metainfo,
+                            mean_rgb=(0.4914, 0.4822, 0.4465),
+                            std_rgb=(0.2023, 0.1994, 0.2010),
+                            jitter_param=None):
+    assert (ds_metainfo is not None)
+    assert (ds_metainfo.input_image_size[0] == 32)
+    return transforms.Compose([
+        transforms.RandomCrop(
+            size=32,
+            padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(
+            brightness=jitter_param,
+            contrast=jitter_param,
+            saturation=jitter_param),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=mean_rgb,
+            std=std_rgb)
+    ])
+
+def cifar10_val_transform(ds_metainfo,
+                          mean_rgb=(0.4914, 0.4822, 0.4465),
+                          std_rgb=(0.2023, 0.1994, 0.2010)):
+    assert (ds_metainfo is not None)
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=mean_rgb,
+            std=std_rgb)
+    ])
+
+def x_u_split(args, labels):
+    label_per_class = args.num_labeled // args.num_classes
+    labels = np.array(labels)
+    labeled_idx = []
+    # unlabeled data: all data (https://github.com/kekmodel/FixMatch-pytorch/issues/10)
+    unlabeled_idx = np.array(range(len(labels)))
+    for i in range(args.num_classes):
+        idx = np.where(labels == i)[0]
+        idx = np.random.choice(idx, label_per_class, False)
+        labeled_idx.extend(idx)
+    labeled_idx = np.array(labeled_idx)
+    assert len(labeled_idx) == args.num_labeled
+
+    if args.expand_labels or args.num_labeled < args.batch_size:
+        num_expand_x = math.ceil(
+            args.batch_size * args.eval_step / args.num_labeled)
+        labeled_idx = np.hstack([labeled_idx for _ in range(num_expand_x)])
+    np.random.shuffle(labeled_idx)
+    return labeled_idx, unlabeled_idx
+
+def get_cifar10(args, root, Transform_un):
+    transform_labeled = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(size=32,
+                              padding=int(32*0.125),
+                              padding_mode='reflect'),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914,0.4822,0.4465), std=(0.2471,0.2435,0.2616))
+    ])
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914,0.4822,0.4465), std=(0.2471,0.2435,0.2616))
+    ])
+    base_dataset = CIFAR10(root, train=True, download=True)
+
+    train_labeled_idxs, train_unlabeled_idxs = x_u_split(
+        args, base_dataset.targets)
+
+    train_labeled_dataset = CIFAR10SSL(
+        root, train_labeled_idxs, train=True,
+        transform=transform_labeled)
+
+    train_unlabeled_dataset = CIFAR10SSL(
+        root, train_unlabeled_idxs, train=True,
+        transform=Transform_un(mean=(0.4914,0.4822,0.4465), std=(0.2471,0.2435,0.2616)))
+
+    test_dataset = CIFAR10(
+        root, train=False, transform=transform_val, download=False)
+
+    return train_labeled_dataset, train_unlabeled_dataset, test_dataset
+
+
